c737fcfb7c56ea2878b3c2e6c6a9b1e23686e8cf
fernandalopez13 <cb16.lopez.pineda.marifer@gmail.com>
2019-05-29 14:47:40 -0500

    2 subir

--- js/main.js
+++ js/main.js
@@ -3,26 +3,26 @@ var audio;
 //Hide Pause Initially
 $('#pause').hide();
 
-//initializer-Play First Song
+//Initializer-Play First Song
 initAudio($('#playlist li:first-child'));
 
-function initAudio(element){
+function initAudio(element) {
 	var song = element.attr('song');
 	var title = element.text();
 	var cover = element.attr('cover');
 	var artist = element.attr('artist');
 	
-	//Create a New Audio Object
+	//Create a new Audio Object
 	audio = new Audio('media/' + song);
 	
 	if(!audio.currentTime){
 		$('#duration').html('0.00');
 	}
 	
-	$('#audio-player.title').text(title);
-	$('#audio-player.artist').text(artist);
+	$('#audio-player .title').text(title);
+	$('#audio-player .artist').text(artist);
 	
-	//insert Cover Image
+	//Insert Cover Image
 	$('img.cover').attr('src','images/covers/' + cover);
 	
 	$('#playlist li').removeClass('active');
@@ -30,8 +30,8 @@ function initAudio(element){
 }
 
 
-//Play Button
-$('#play').click(function (){
+//play Button
+$('#play').click(function(){
 	audio.play();
 	$('#play').hide();
 	$('#pause').show();
@@ -40,24 +40,24 @@ $('#play').click(function (){
 });
 
 //Pause button
-$('#pause').click(function (){
+$('#pause').click(function(){
 	audio.pause();
 	$('#pause').hide();
 	$('#play').show();
 });
 
-//stop Button
-$('#stop').click(function (){
+//Stop Button
+$('#stop').click(function(){
 	audio.pause();
-	audio.currentTime= 0;
-	$('#pause').hide();
-	$('#play').show();
+	audio.currentTime = 0;
+	$('#pause').hide();	
+    $('#play').show();
 	$('#duration').fadeOut(400);
 });
 
-//Playlist song click
+//Playlist Song Click
 $('#playlist li').click(function (){
-	audio.pause ();
+	audio.pause();
 	initAudio($(this));
 	$('#play').hide();
 	$('#pause').show();
@@ -66,33 +66,26 @@ $('#playlist li').click(function (){
 	showDuration();
 });
 
-//Volume control
+//Volumen Control
 $('#volume').change(function(){
 	audio.volume = parseFloat (this.value/10);
 });
 
 //Time Duration
-function showDuration(){
-	$(audio).bind('timeupdate',function(){
-		//Get hours and minutes
-		var s = parseInt(audio.currentTime%60);
-		var m = parseInt((audio.currentTime/60)%60);
-		//Add 0 if seconds less than 10
-		if (s < 10){
-			s = '0' + s;
-		}
-		$('#duration').html(m + '.' + s);
-		var value=0;
-		if(audio.currentTime > 0) {
-			value = Math.floor((100/audio.duration) * audio.currentTime);
-		}
-		$('#progress').css('width',value+'%');
-	});
+function showDuration(){	
+$(audio).bind('timeupdate',function(){
+	//Get hours and minutes
+	var s = parseInt(audio.currentTime %60);
+	var m = parseInt((audio.currentTime /60)%60);
+	//Add 0 if seconds less than 10
+	if (s < 10) {
+		s = '0' + s;
+	}
+$('#duration').html(m + '.' + s);
+var value = 0 ;
+if (audio.currentTime > 0) {
+	value = Math.floor((100 / audio.duration)*audio.currentTime);
 }
-
-	
-		
-		
-
-
-
+$('#progress').css('width',value + '%');
+});
+}
